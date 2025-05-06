418ab78d551b2af4aa79dc58b9bd89e2f2b0098e
denkiri <denkiri@users.noreply.github.com>
2025-05-06 14:22:39 +0000

    deploy: 21a763082e824c60f00dc65fc6c7a5719ad74c1e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1942175464"
+    serviceWorkerVersion: "218492780"
   }
 });
